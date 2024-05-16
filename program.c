76f140f820c196996edf7c64ce44ee89a3f0bb91
sentinerugotou <shainingtakeda@gmail.com>
2024-05-16 12:00:48 +0900

    second

--- program.c
+++ program.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
 int main(void){
     printf("hello git I'm Haruki. no.1 you friend!");
+    int i;
+
+    for ( i = 0; i < 100; i++)
+    {
+        printf("second");
+    }
+
 }
